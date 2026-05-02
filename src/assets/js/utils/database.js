979262198544fd077f7e5112b37a3821526c06aa
ImSanty <santiaguero936@gmail.com>
2026-05-02 20:39:50 -0300

    feat: implement encrypted database utility and update project dependencies

--- src/assets/js/utils/database.js
+++ src/assets/js/utils/database.js
@@ -3,11 +3,43 @@
  * @license CC-BY-NC 4.0 - https://creativecommons.org/licenses/by-nc/4.0
  */
 
-const { NodeBDD, DataType } = require('node-bdd');
-const nodedatabase = new NodeBDD();
+const fs = require('fs');
+const path = require('path');
+const crypto = require('crypto');
 const { ipcRenderer } = require('electron');
 
-let dev = process.env.NODE_ENV === 'dev';
+// Encryption configuration
+const ALGORITHM = 'aes-256-cbc';
+const SECRET_KEY = crypto.createHash('sha256').update('VoxeliteLauncher').digest();
+const IV_LENGTH = 16;
+
+/**
+ * Encrypts a string
+ */
+function encrypt(text) {
+  const iv = crypto.randomBytes(IV_LENGTH);
+  const cipher = crypto.createCipheriv(ALGORITHM, SECRET_KEY, iv);
+  let encrypted = cipher.update(text);
+  encrypted = Buffer.concat([encrypted, cipher.final()]);
+  return iv.toString('hex') + ':' + encrypted.toString('hex');
+}
+
+/**
+ * Decrypts a string
+ */
+function decrypt(text) {
+  try {
+    const textParts = text.split(':');
+    const iv = Buffer.from(textParts.shift(), 'hex');
+    const encryptedText = Buffer.from(textParts.join(':'), 'hex');
+    const decipher = crypto.createDecipheriv(ALGORITHM, SECRET_KEY, iv);
+    let decrypted = decipher.update(encryptedText);
+    decrypted = Buffer.concat([decrypted, decipher.final()]);
+    return decrypted.toString();
+  } catch (error) {
+    return null;
+  }
+}
 
 async function resolveDatabasePath() {
   const basePath = await ipcRenderer.invoke('database-path');
@@ -15,68 +47,108 @@ async function resolveDatabasePath() {
 }
 
 class database {
+  constructor() {
+    this.basePath = null;
+  }
+
+  async ensurePath() {
+    if (!this.basePath) {
+      this.basePath = await resolveDatabasePath();
+      if (!fs.existsSync(this.basePath)) {
+        fs.mkdirSync(this.basePath, { recursive: true });
+      }
+    }
+  }
+
+  getFilePath(tableName) {
+    return path.join(this.basePath, `${tableName}.vdb`);
+  }
+
+  async loadTable(tableName) {
+    await this.ensurePath();
+    const filePath = this.getFilePath(tableName);
+    if (!fs.existsSync(filePath)) {
+      return [];
+    }
+    const encryptedData = fs.readFileSync(filePath, 'utf8');
+    const jsonData = decrypt(encryptedData);
+    if (!jsonData) return [];
+    try {
+      return JSON.parse(jsonData);
+    } catch (e) {
+      return [];
+    }
+  }
+
+  async saveTable(tableName, data) {
+    await this.ensurePath();
+    const filePath = this.getFilePath(tableName);
+    const jsonData = JSON.stringify(data);
+    const encryptedData = encrypt(jsonData);
+    fs.writeFileSync(filePath, encryptedData, 'utf8');
+  }
+
   async creatDatabase(tableName, tableConfig) {
-    const basePath = await resolveDatabasePath();
-    return await nodedatabase.intilize({
-      databaseName: 'Databases',
-      fileType: dev ? 'sqlite' : 'db',
-      tableName: tableName,
-      path: basePath,
-      tableColumns: tableConfig
-    });
+    await this.ensurePath();
+    return tableName;
   }
 
   async getDatabase(tableName) {
-    return await this.creatDatabase(tableName, {
-      json_data: DataType.TEXT.TEXT
-    });
+    return tableName;
   }
 
   async createData(tableName, data) {
-    let table = await this.getDatabase(tableName);
-    data = await nodedatabase.createData(table, {
+    const table = await this.loadTable(tableName);
+    const id = table.length > 0 ? Math.max(...table.map((i) => i.id || 0)) + 1 : 1;
+    
+    const record = {
+      id: id,
       json_data: JSON.stringify(data)
-    });
-    let id = data.id;
-    data = JSON.parse(data.json_data);
-    data.ID = id;
-    return data;
+    };
+    
+    table.push(record);
+    await this.saveTable(tableName, table);
+    
+    const result = JSON.parse(record.json_data);
+    result.ID = record.id;
+    return result;
   }
 
   async readData(tableName, key = 1) {
-    let table = await this.getDatabase(tableName);
-    let data = await nodedatabase.getDataById(table, key);
-    if (data) {
-      let id = data.id;
-      data = JSON.parse(data.json_data);
-      data.ID = id;
+    const table = await this.loadTable(tableName);
+    const record = table.find((i) => i.id == key);
+    if (record) {
+      const data = JSON.parse(record.json_data);
+      data.ID = record.id;
+      return data;
     }
-    return data ? data : undefined;
+    return undefined;
   }
 
   async readAllData(tableName) {
-    let table = await this.getDatabase(tableName);
-    let data = await nodedatabase.getAllData(table);
-    return data.map((info) => {
-      let id = info.id;
-      info = JSON.parse(info.json_data);
-      info.ID = id;
-      return info;
+    const table = await this.loadTable(tableName);
+    return table.map((record) => {
+      const data = JSON.parse(record.json_data);
+      data.ID = record.id;
+      return data;
     });
   }
 
   async updateData(tableName, data, key = 1) {
-    let table = await this.getDatabase(tableName);
-    await nodedatabase.updateData(
-      table,
-      { json_data: JSON.stringify(data) },
-      key
-    );
+    const table = await this.loadTable(tableName);
+    const index = table.findIndex((i) => i.id == key);
+    if (index !== -1) {
+      table[index].json_data = JSON.stringify(data);
+      await this.saveTable(tableName, table);
+    }
   }
 
   async deleteData(tableName, key = 1) {
-    let table = await this.getDatabase(tableName);
-    await nodedatabase.deleteData(table, key);
+    const table = await this.loadTable(tableName);
+    const filtered = table.filter((i) => i.id != key);
+    if (filtered.length !== table.length) {
+      await this.saveTable(tableName, filtered);
+    }
   }
 }
 
